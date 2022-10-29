a8b3b737e901656ca224bb1da75cf9289072e264
Shoterz <95153408+Shoterz@users.noreply.github.com>
2022-10-29 14:26:30 +0800

    Add files via upload

--- popup.js
+++ popup.js
@@ -13,27 +13,8 @@ function togglePopLicense2(content){
 function togglePopLicense3(content){
 	document.getElementById("popup-5").classList.toggle("active");
 }
-function togglePopMoreInfo(content){
+function togglePopupMoreInfo(content){
 	document.getElementById("popup-6").classList.toggle("active");
 }
 
 
-if(window.scrollTop() > 0){
-  document.getElementById('headers').style.position="fixed";
-}
-
-function openmenu()
-				{
-					document.getElementById("side-menu").style.display="block";
-					document.getElementById("menu-btn").style.display="none";
-					document.getElementById("close-btn").style.display="block";
-				}
-			function closemenu()
-				{
-					document.getElementById("side-menu").style.display="none";
-					document.getElementById("menu-btn").style.display="block";
-					document.getElementById("close-btn").style.display="none";
-				}
-				
-				
-				
