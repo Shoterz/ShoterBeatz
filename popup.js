5f822c71e4989fc29220bf3b35c8309f8d00d71f
Shoterz <95153408+Shoterz@users.noreply.github.com>
2022-11-03 09:58:02 +0800

    Add files via upload

--- popup.js
+++ popup.js
@@ -18,3 +18,52 @@ function togglePopupMoreInfo(content){
 }
 
 
+function message(){
+	var email = document.getElementById('email');
+    const success = document.getElementById('success');
+    const danger = document.getElementById('danger');
+
+    if(email.value === ''){
+        
+    }
+    else{
+        setTimeout(() => {
+            email.value = '';
+        }, 10000);
+
+        success.style.display = 'block';
+    }
+
+
+    setTimeout(() => {
+        danger.style.display = 'none';
+        success.style.display = 'none';
+    }, 10000);
+}
+
+ var form = document.getElementById('emailform');
+        form.addEventListener("submit", e => {
+          e.preventDefault();
+          fetch(form.action, {
+              method : "POST",
+              body: new FormData(document.getElementById("emailform")),
+          }).then(
+              response => response.json()
+          ).then((html) => {
+            // you can put any JS code here
+
+          });
+        });
+		
+function SubmitForm()
+{
+	showResultDiv();
+	document.forms['search'].action='https://sheetdb.io/api/v1/2fnor4g03g8cb';
+    document.forms['search'].submit();
+	
+    document.forms['search'].action='mail_handler.php';
+    document.forms['search'].submit();
+	
+
+    return false;
+}
