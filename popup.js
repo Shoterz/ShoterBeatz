86663b55e5ac7d5c7b1c7b7eb7e8e735fbabb60b
Shoterz <95153408+Shoterz@users.noreply.github.com>
2023-01-14 19:33:39 +0800

    Add files via upload

--- popup.js
+++ popup.js
@@ -74,7 +74,7 @@ if(email.value.match(mailformat))
 {
 setTimeout(() => {
             email.value = '';
-        }, 10000);
+        }, 5000);
 
         success.style.display = 'block';
 		 window.open('https://drive.google.com/drive/folders/1zeSxRRtkz9s6P8MNd_Z_bCCMmaSw37v0?usp=share_link', '_self');
@@ -88,7 +88,7 @@ else
 	setTimeout(() => {
         danger.style.display = 'none';
         success.style.display = 'none';
-    }, 10000);
+    }, 5000);
 }
 
 function message1(){
